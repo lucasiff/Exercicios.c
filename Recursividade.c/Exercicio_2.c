c8f502b5057bb83f4fa022938217e8b06e26186a
lucasiff <100242780+lucasiff@users.noreply.github.com>
2022-03-28 04:47:05 -0300

    Update Exercicio_2.c

---
+++ Recursividade.c/Exercicio_2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int fibonacci(int n)
+{
+  if(n==1||n==2)
+  {
+    return 1;
+  }
+  else
+  {
+    return fibonacci(n-1)+fibonacci(n-2); 
+  }
+}
+
+int main()
+{
+  int contador;
+
+  for(contador=1;contador<=10;contador++)
+  {
+    printf("\n%d\n",fibonacci(contador));
+  }
+
+return 0;
+}
