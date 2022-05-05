cfc2bc91569d5450249cad51bfed03d78f425272
lucasiff <100242780+lucasiff@users.noreply.github.com>
2022-05-05 00:11:57 -0300

    Add files via upload

--- Pilha_<Lucas da Silva Gomes Oliveira e Yuri Alcantara Pacheco De Souza>/Exercicio_1.c
+++ Pilha_<Lucas da Silva Gomes Oliveira e Yuri Alcantara Pacheco De Souza>/Exercicio_1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Pilha.h"
 
+int main()
+{
+  struct spilha pilha;
+  int i;
+
+  init(&pilha);
+
+  printf("Executando \n\n");
+
+  push(&pilha,'A');
+  push(&pilha,'B');
+  push(&pilha,'C');
+  push(&pilha,'D');
+  push(&pilha,top (&pilha));
+  pop(&pilha);
+  push(&pilha,pop (&pilha));
+  pop(&pilha);
+  for (i=pilha.topo-1; i>=0; i--)
+ { printf("%c\n",pilha.vetor[i]); }
+
+}
