3ec7365ef1272f365a8805b6c2e2341f834b62e9
lucasiff <100242780+lucasiff@users.noreply.github.com>
2022-12-15 14:28:56 -0300

    Add files via upload

---
+++ estruturas sequenciais/Exercicio_8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  float peso,altura,imc;
+
+  printf("Digite o seu peso:");
+  scanf("%f",&peso);
+  printf("Digite a sua altura:");
+  scanf("%f",&altura);
+  imc=peso/altura*altura;
+  printf("O seu IMC e = %f\n",imc);
+
+  return 0;
+}
