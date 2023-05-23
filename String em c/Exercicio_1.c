fa36c3f99e88eaf274e99802a370627790807842
lucasiff <100242780+lucasiff@users.noreply.github.com>
2023-05-23 15:30:46 -0300

    Add files via upload

--- String em c/Exercicio_1.c
+++ String em c/Exercicio_1.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
+#include<string.h>
 
+int main()
+{
+  char nome[100],sexo[100],contador=0;
+  int totalM=0,totalF=0;
+
+  for(contador=1;contador<=10;contador++)
+  {
+    printf("Digite o seu nome:");
+    scanf("%s",nome);
+    printf("Digite o seu sexo:");
+    scanf("%s",sexo);
+
+    if(strcmp(sexo,"masculino")==0)
+    {
+      totalM++;
+    }
+    else
+    {
+      if(strcmp(sexo,"feminino")==0)
+      {
+        totalF++;
+      }
+    }
+  }
+  printf("\ntotal de pessoas do sexo masculino:%d\n",totalM);
+  printf("\ntotal de pessoas do sexo feminino:%d\n",totalF);
+
+  return 0;
+}
